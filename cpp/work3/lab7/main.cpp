dc25c8287afa38ef13fe21e212ad0cc1378c9205
Rassska <rasul.yunusov282750@gmail.com>
2021-04-28 09:12:14 +0300

    updated lab7

--- cpp/work3/lab7/main.cpp
+++ cpp/work3/lab7/main.cpp
@@ -7,7 +7,7 @@ private:
     
 public:
     student() = default;
-    virtual void foo(){}
+    virtual void getInfoAboutStudent() {}
     virtual ~student() = default;
 };
 
@@ -39,8 +39,15 @@ private:
     std::vector<std::size_t> m_allStudentStipends;
 public:
     A_Student()= default;
-    std::vector<std::size_t> getAllStudentStipends() const {
+    std::vector<std::size_t>& getAllStudentStipends() {
         return m_allStudentStipends;
+    }
+    void getInfoAboutStudent() override {
+        std::cout << "Here is the list of stipends: \n";
+        for (std::size_t i = 0; i < m_allStudentStipends.size(); i++) {
+            std::cout << m_allStudentStipends[i] << ' ';
+        }
+        std::cout << "\n\n";
     }
     ~A_Student()= default;
 };
@@ -51,8 +58,16 @@ private:
     std::vector<std::size_t> m_allStudentStipends;
 public:
     B_Student()= default;
-    std::vector<std::size_t> getAllStudentStipends() const {
+    std::vector<std::size_t>& getAllStudentStipends() {
         return m_allStudentStipends;
+    }
+
+     void getInfoAboutStudent() override {
+        std::cout << "Here is the list of stipends: \n";
+        for (std::size_t i = 0; i < m_allStudentStipends.size(); i++) {
+            std::cout << m_allStudentStipends[i] << ' ';
+        }
+        std::cout << "\n\n";
     }
     ~B_Student()= default;
 };
@@ -63,20 +78,42 @@ private:
     std::vector<std::size_t> m_allStudentStipends;
 public:
     C_Student()= default;
-    std::vector<std::size_t> getAllStudentStipends() const {
+    std::vector<std::size_t>& getAllStudentStipends()  {
         return m_allStudentStipends;
     }
+
+    void getInfoAboutStudent() override {
+        std::cout << "Here is the list of stipends: \n";
+        for (std::size_t i = 0; i < m_allStudentStipends.size(); i++) {
+            std::cout << m_allStudentStipends[i] << ' ';
+        }
+        std::cout << "\n\n";
+    }
+
     ~C_Student()= default;
 };
 
 class D_Student : public non_stipendStudents
 {
 private:
-    std::vector<std::size_t> m_academicDebts;
+    std::vector<std::time_t> m_academicDebts;
 public:
     D_Student()= default;
-    std::vector<std::size_t> getAcademicDebts() const {
+    std::vector<std::time_t>& getAcademicDebts() {
         return m_academicDebts;
+    }
+
+    void getInfoAboutStudent() override {
+        if (m_academicDebts.size() > 3) {
+            std::cout << "Current student had been removed by DeanOffice!" << "\n\n";
+            return;
+        }
+        std::cout << "Here is the list of debt dates: \n";
+        for (std::size_t i = 0; i < m_academicDebts.size(); i++) {
+            std::cout << std::asctime(std::localtime(&m_academicDebts[i]));
+        }
+        std::cout << std::endl;
+        
     }
     ~D_Student()= default;
 };
@@ -116,12 +153,12 @@ public:
                 if (tempDNonStipendStudent->getAcademicDebts().size() <= 3) {
                     tempDNonStipendStudent->getAcademicDebts().push_back(std::time(nullptr)); // Добавляем в список дату для пересдач с вектор, содержащий все даты передач, только 3 разрешены!
                 } else {
-                    std::cout << "Student has more than 3 attempt to pass exam!\n";
                     currBase->removeStudentFromBase(i); // Отчисляем студента :(
                 }
                 
             } else if (A_Student* tempAStipenStudent = dynamic_cast<A_Student*>(currBase->getAllStudents()[i])) {
                 tempAStipenStudent->getAllStudentStipends().push_back(5000); // Начисление стипендии отличнику в размере 5000! 
+
             } else if (B_Student* tempBStipenStudent = dynamic_cast<B_Student*>(currBase->getAllStudents()[i])) {
                 tempBStipenStudent->getAllStudentStipends().push_back(3000); // Начисление стипендии хорошисту в размере 3000! 
             } else if (C_Student* tempCStipenStudent = dynamic_cast<C_Student*>(currBase->getAllStudents()[i])) {
@@ -143,16 +180,28 @@ int main() {
     studentsDataBase* allStudentsDataBase = new studentsDataBase();
     deanOffice* currDeanOffice = new deanOffice();
 
-    student* Ivanov = new D_Student();
+    student* Ivanov = new A_Student();
     student* Maslov = new D_Student();
+    student* Nekrasov = new B_Student();
     allStudentsDataBase->addStudentToTheBase(Ivanov);
     allStudentsDataBase->addStudentToTheBase(Maslov);
+    allStudentsDataBase->addStudentToTheBase(Nekrasov);
 
     //! Деканат создает репорт, по итогам которого неуспевающим студентам выставляет дату для пересдачи, 
     //! если пересдач было уже 3 на момент создания отчета, студента отчисляют!
     //! А успевающим - начисляет стипендию.
 
     currDeanOffice->createReport(allStudentsDataBase); 
+    currDeanOffice->createReport(allStudentsDataBase); 
+    currDeanOffice->createReport(allStudentsDataBase); 
+    currDeanOffice->createReport(allStudentsDataBase); 
+    currDeanOffice->createReport(allStudentsDataBase); 
+     
+
+    Ivanov->getInfoAboutStudent();
+    Maslov->getInfoAboutStudent();
+    Nekrasov->getInfoAboutStudent();
+
     
 
 
