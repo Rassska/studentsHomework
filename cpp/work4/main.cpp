cd8271f0b1ccad7727997afbfdbf0a3179af559b
Rassska <rasul.yunusov282750@gmail.com>
2021-04-29 15:13:05 +0300

    added work4

--- cpp/work4/main.cpp
+++ cpp/work4/main.cpp
@@ -1,38 +1,103 @@
 #include <iostream>
+#include <vector>
 
-class linkedList
+struct node {
+    int value;
+    node* next;
+};
+struct linkedList
 {
-private:
-    typedef struct node {
-        int value;
-        node* next;
-    } node;
-
-    nodePtr head;
-    nodePtr curr;
-    nodePtr temp;
-
-public:
-    linkedList::linkedList() {
+    node* head;
+    node* last;
+    linkedList() {
         head = nullptr;
-        curr = nullptr;
-        temp = nullptr;
+        last = nullptr;
     }
-    linkedList::~linkedList() = default;
 
-    void addNode(int newValue) {
+    bool isEmpty() {
+        if (head == nullptr) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void insertAsFirstElement(int newValue) {
+
+        node* tempNode = new node();
+        tempNode->value = newValue;
+        tempNode->next = nullptr;
+        head = tempNode;
+        last = tempNode;
+        
 
     }
-    void DeleteNode(int valueToDelete) {
 
+    void insert(int newValue) {
+        if (isEmpty()) {
+            insertAsFirstElement(newValue);
+        } else {
+            node* tempNode = new node();
+            tempNode->value = newValue;
+            tempNode->next = head;
+            head = tempNode;
+            
+        }
     }
-    void printList() {
-
+    void remove() {
+        if (isEmpty()) {
+            std::cout << "The linked list is already empty.\n";
+        } else if (head == last) {
+            delete head;
+            head = last = nullptr;
+        } else {
+            node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    void showList() {
+        if (isEmpty()) {
+            std::cout << "The list is empty!\n";
+        } else {
+            node* curr = head;
+            while(curr != nullptr) {
+                std::cout << curr->value << ' ';
+                curr = curr->next;
+            }
+            std::cout << '\n';
+        }
     }
 };
 
-
-
 int main () {
+
+    std::size_t n;
+    std::cout << "Please, input the size of your array!\n";
+    std::cin >> n;
+    std::vector<linkedList> vectorOfLinkedLists(n);
+    std::size_t keyPosition = 100;
     
+    vectorOfLinkedLists[keyPosition % n].insert(50);
+    keyPosition = 20;
+    vectorOfLinkedLists[keyPosition % n].insert(500);
+    keyPosition = 34;
+    vectorOfLinkedLists[keyPosition % n].insert(5007);
+
+    vectorOfLinkedLists[keyPosition % n].remove();
+    keyPosition = 20;
+    vectorOfLinkedLists[keyPosition % n].remove();
+    keyPosition = 100;
+    vectorOfLinkedLists[keyPosition % n].showList();
+
+    keyPosition = 34;
+    vectorOfLinkedLists[keyPosition % n].showList();
+
+    keyPosition = 20;
+    vectorOfLinkedLists[keyPosition % n].showList();
+
+
+    
+
 }
+
